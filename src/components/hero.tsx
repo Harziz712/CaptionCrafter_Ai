bc7b41089a0c16205ef4ead9fc0b14f2b2a36f61
Akanbi AbdulAzeez <akanbiazeez117@gmail.com>
2025-04-23 14:10:51 +0100

    fix: description button

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <div className="w-full min-h-[500px] flex flex-col items-center justify-center px-4 py-40  text-center relative">
       {/* <Badge variant="outline" className="bg-blue-50/40 text-gray-800 px-4 py-2 rounded-full text-sm mb-8 border-2 border-blue-400"> */}
-   <Badge variant="outline" className="animatedButton rounded-full xl:top-[15rem] md:top-[15rem] top-20">
+   <Badge variant="outline" className="animatedButton rounded-full xl:top-20 md:top-15 top-20">
    <h2 className="animatedButtonText rounded-full bg-blue-50/40 text-white "> Powered by AI — Built for Creators & Marketers</h2>
       </Badge>
 
