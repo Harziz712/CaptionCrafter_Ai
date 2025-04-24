fe6604f4ffc90a1f6b90578eabfdd04741c22c59
Akanbi AbdulAzeez <akanbiazeez117@gmail.com>
2025-04-24 22:12:36 +0100

    added a footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
 import Captionarea from "../components/captionarea"
+import Footer from "../components/footer"
 import Hero from "../components/hero"
 import Navbar from "../components/navbar"
 
 const Index = () => {
   return (
-    <div className=" min-h-[100%] pb-20">
+    <div className=" min-h-[100%]">
         <Navbar/>
         <Hero/>
         <Captionarea/>
+        <Footer/>
     </div>
   )
 }
